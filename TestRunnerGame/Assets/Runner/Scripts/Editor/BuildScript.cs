3198bd1d9ad8762fb4cfe2282db4d9c7ddd0a38b
ZengXianhan <zengxianhan93@gmail.com>
2023-04-05 22:52:37 +0800

    Init commit game

---
+++ TestRunnerGame/Assets/Runner/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+
+public class BuildScript
+{
+    public static void BuildWebGL()
+    {
+        BuildPipeline.BuildPlayer(new string[] { "Assets/Scenes/SampleScene.unity" }, "Builds/WebGL", BuildTarget.WebGL, BuildOptions.None);
+    }
+}
